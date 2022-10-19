d9e269da048b0cae07223f6605a14c0d7407fcd6
Kim Harribaud <kim.harribaud@etu.umontpellier.fr>
2022-10-19 23:38:34 +0200

    commit

---
+++ js/click.js
@@ -0,0 +1,6 @@
+
+document.getElementById("click").addEventListener("click", function(){
+    document.getElementById("menu").classList.toggle("menu1");
+    document.getElementById("menu").classList.toggle("menu_view");
+    document.body.classList.toggle("bodyfix");
+})
